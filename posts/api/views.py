ad1fe3e7246213e345e31fcc5598ec518763a006
PierreChavez <pchavez@vumigroup.com>
2024-07-09 11:21:03 -0500

    model view set

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,17 +1,42 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework import status
 from posts.models import Post
 from posts.api.serializers import PostSerializer
 
-class PostApiView(APIView):
-    def get(self, request):
-        serializer = PostSerializer(Post.objects.all(), many=True)
-        return Response(status=status.HTTP_200_OK, data={'data': serializer.data})
+class PostModelViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
 
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(status=status.HTTP_400_BAD_REQUEST, data={'message': 'Invalid data', 'errors': serializer.errors})
-        serializer.save()
-        return Response(status=status.HTTP_201_CREATED, data={'message': 'Post created', 'data': serializer.data})
+# class PostViewSet(ViewSet):
+#     def list(self, request):
+#         serializer = PostSerializer(Post.objects.all(), many=True)
+#         return Response(status=status.HTTP_200_OK, data={'data': serializer.data})
+#
+#     def retrieve(self, request, pk=int):
+#         post = Post.objects.get(pk=pk)
+#         serializer = PostSerializer(post)
+#         return Response(status=status.HTTP_200_OK, data={'data': serializer.data})
+#
+#     def create(self, request):
+#         serializer = PostSerializer(data=request.data)
+#         if not serializer.is_valid():
+#             return Response(status=status.HTTP_400_BAD_REQUEST, data={'message': 'Invalid data', 'errors': serializer.errors})
+#         serializer.save()
+#         return Response(status=status.HTTP_201_CREATED, data={'message': 'Post created', 'data': serializer.data})
+        
+# class PostApiView(APIView):
+#     def get(self, request):
+#         serializer = PostSerializer(Post.objects.all(), many=True)
+#         return Response(status=status.HTTP_200_OK, data={'data': serializer.data})
+#
+#     def post(self, request):
+#         serializer = PostSerializer(data=request.data)
+#         if not serializer.is_valid():
+#             return Response(status=status.HTTP_400_BAD_REQUEST, data={'message': 'Invalid data', 'errors': serializer.errors})
+#         serializer.save()
+#         return Response(status=status.HTTP_201_CREATED, data={'message': 'Post created', 'data': serializer.data})
+
+
